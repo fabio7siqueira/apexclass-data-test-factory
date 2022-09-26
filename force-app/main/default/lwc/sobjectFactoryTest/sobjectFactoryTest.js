47c28288370057736dba7bb64eda30c7de736e56
Fabio Lima <fabio7siqueira@gmail.com>
2022-09-25 23:16:20 -0300

    Desenvolvimento 2022-09-25

--- force-app/main/default/lwc/sobjectFactoryTest/sobjectFactoryTest.js
+++ force-app/main/default/lwc/sobjectFactoryTest/sobjectFactoryTest.js
@@ -1,24 +1,58 @@
 import { LightningElement, api, track, wire } from 'lwc';
 import generateMethodToFactorySobject from '@salesforce/apex/SobjectFactoryTest.generateMethodToFactorySobject';
+import { CurrentPageReference } from 'lightning/navigation';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class SobjectFactoryTest extends LightningElement {
-    @api recordId;
-    @track factoryMethod = '';
-    @track sobjectId = '';
+    @track recordId;
+    @track factoryMethod = undefined;
+    @track loading = undefined;
+
+    currentPageReference = null; 
+    urlStateParameters = null;
 
     connectedCallback() {
         
     }
 
-    handleChange(event) {
-        this.sobjectId = event.target.value;
-        
-        generateMethodToFactorySobject({recordId: event.target.value})
-            .then(result => {
-                this.factoryMethod = result;
-            })
-            .catch(error => {
-                this.factoryMethod = JSON.stringify(error);
-            });
+    @wire(CurrentPageReference)
+    getPageReferenceParameters(currentPageReference) {
+        if (currentPageReference) {
+            this.recordId = currentPageReference.attributes.recordId || null;
+            this.factoryMethod = '';
+        }
+    }
+
+    handleClick(event) {
+        this.requestApex(this.recordId);
+    }
+
+    async requestApex(sObjectId) {
+        this.loading = true;
+
+        await generateMethodToFactorySobject({recordId: sObjectId})
+        .then(result => {
+            this.factoryMethod = result;
+            
+            this.dispatchEvent(new ShowToastEvent({
+                title: 'Success!', 
+                variant: 'success', 
+                mode: 'pester', 
+                message: 'Method generated with success!'
+            }));
+        })
+        .catch(error => {
+            this.factoryMethod = '';
+            this.recordId = '';
+
+            this.dispatchEvent(new ShowToastEvent({
+                title: 'Error!', 
+                variant: 'error', 
+                mode: 'sticky', 
+                message: `Something went wrong ::: ${error.body.message}`
+            }));
+        });
+
+        this.loading = false;
     }
 }
